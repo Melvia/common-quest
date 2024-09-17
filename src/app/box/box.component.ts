b2972e06a2b7a62e8fad9576f1a37806a91824c8
Nata <melvia.nr@gmail.com>
2024-09-17 21:19:40 +0500

    feat: revision of stick

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -21,20 +21,12 @@ export class BoxComponent implements OnInit {
   @Input() imgUrl = '';
   protected renderer = inject(Renderer2);
   protected elementRef = inject(ElementRef);
-  sticks: HTMLElement[]=[];
-  active = signal(false);
+  sticks: HTMLElement[] = [];
+  active = signal(true);
 
   @HostListener('click') onClick() {
     this.active.update(value => !value);
-    this.sticks.forEach(function (span) {
-      span.style.setProperty('--x', Math.floor(Math.random() * 400 - 200) + 'px');
-    })
-    this.sticks.forEach(function (span) {
-      span.style.setProperty('--r', Math.floor(Math.random() * 360 - 0) + 'deg');
-    })
-
-
- //   transform: translate(var(--x)) rotate(var(--r));
+    this.sticks.forEach((stick) => this.stickTransform(stick));
   }
 
 
@@ -42,16 +34,23 @@ export class BoxComponent implements OnInit {
     for (let i = 0; i < 200; i++) {
       const stick = this.renderer.createElement('span') as HTMLElement;
       this.sticks.push(stick);
-      stick.style.top = `${i * 1.5}px`;
-      stick.style.backgroundPositionY = `${i * -1.5}px`;
-      stick.style.transitionDelay = (Math.random() * 1.5) + 0 + 's';
-      stick.style.backgroundImage = `url(${this.imgUrl})`;
+      this.setProperties(stick, i);
       this.renderer.appendChild(this.elementRef.nativeElement, stick);
+      this.stickTransform(stick);
     }
-
-
   }
 
+  setProperties(stick: HTMLElement, index: number): void {
+    stick.style.top = `${index * 1.5}px`;
+    stick.style.backgroundPositionY = `${index * -1.5}px`;
+    stick.style.transitionDelay = (Math.random() * 1.5) + 0 + 's';
+    stick.style.backgroundImage = `url(${this.imgUrl})`;
+  }
 
+  stickTransform(stick: HTMLElement) {
+    const translateValue = this.active() ? Math.floor(Math.random() * 400 - 200) + 'px' : '0';
+    const rotateValue = this.active() ? Math.floor(Math.random() * 360 - 0) + 'deg' : '0';
+    stick.style.transform = `translate(${translateValue}) rotate(${rotateValue})`;
+  }
 
 }
