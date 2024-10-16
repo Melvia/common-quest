44678e4f86f0f862ac2f97342c2099a2abe964e0
Nata <melvia.nr@gmail.com>
2024-10-16 22:02:46 +0500

    feat: improve lattice

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {NgClass} from "@angular/common";
 
 const FIELD_SIZE = 6;
+
 @Component({
   selector: 'app-grid',
   standalone: true,
@@ -11,20 +12,26 @@ const FIELD_SIZE = 6;
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss'
 })
-export class GridComponent implements OnInit{
+export class GridComponent implements OnInit {
   items: boolean[] = [];
-  lionCoords = [[1,1], [2,1], [3,1], [0,4]];
-  crownCoords = [[2,2], [3,2], [2,3], [3,3]];
+  lionCoords = [[1, 1], [2, 1], [3, 1], [0, 4]];
+  crownCoords = [[2, 2], [3, 2], [2, 3], [3, 3]];
   fieldSize = FIELD_SIZE;
+  //все координаты
+  coords: number[][][] = [];
 
-  verticalBorders: number[] =[];
+  verticalBorders: number[] = [];
   horizontalBorders: number[] = [];
 
 
   ngOnInit(): void {
-    for (let i = 0; i<this.fieldSize + 1; i++) {
+    for (let i = 0; i < this.fieldSize + 1; i++) {
       this.horizontalBorders.push(i);
       this.verticalBorders.push(i);
+      this.coords[i] = [];
+      for (let j = 0; j < this.fieldSize + 1; j++) {
+        this.coords[i][j] = [i, j];
+      }
     }
   }
 
@@ -32,23 +39,19 @@ export class GridComponent implements OnInit{
     return x + y * (FIELD_SIZE);
   }
 
-  hasLionClass(x:number, y:number) {
-    return (!!this.lionCoords?.some(coord => coord[0]===x && coord[1]===y));
+  hasLionClass(x: number, y: number) {
+    return (!!this.lionCoords?.some(coord => coord[0] === x && coord[1] === y));
   }
-  hasCrownClass(x:number, y:number) {
-    return (!!this.crownCoords?.some(coord => coord[0]===x && coord[1]===y));
+
+  hasCrownClass(x: number, y: number) {
+    return (!!this.crownCoords?.some(coord => coord[0] === x && coord[1] === y));
   }
 
   toggleClass(e: any) {
-
+    console.log(e.target.dataset);
     e.target.classList.toggle('active');
+
   }
-
-
-
-
-
-
 
 
 }
